9ff2c34135c48f513060a5a7d72d7fb1b89f1af5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-21 00:41:22 +0800

    add conditional operator demonstration in conditional_op.dart

---
+++ bin/conditional_op.dart
@@ -0,0 +1,29 @@
+void
+main() {
+  var a = 1;
+  var b = 2;
+
+  a <
+          b
+      ? print(
+        "$a is smaller ",
+      )
+      : print(
+        "$b is smaller",
+      );
+
+  var first =
+      30;
+  var second =
+      90;
+
+  var smallerNumber =
+      first <
+              second
+          ? first
+          : second;
+
+  print(
+    smallerNumber,
+  );
+}
