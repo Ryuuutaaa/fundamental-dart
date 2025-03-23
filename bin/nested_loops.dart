2030fe3615cb898535d686ea30c56adfb4d4c65f
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-23 23:12:34 +0800

    refactor: replace nested while loops with do-while loops in nested_loops.dart

--- bin/nested_loops.dart
+++ bin/nested_loops.dart
@@ -1,12 +1,12 @@
 void main() {
   var i = 0;
 
-  while (i < 11) {
+  do {
     var j = 0;
-    while (j < 11) {
-      print('$i*$j, $j-Hi');
+    do {
+      print('$i*$j=${i * j}');
       j++;
-    }
+    } while (j < 11);
     i++;
-  }
+  } while (i < 11);
 }
