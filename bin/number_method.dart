9af10f07b3cbd8cf043976ee5d294f1cbcf91282
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-24 00:20:44 +0800

    feat: add number method examples in number_method.dart

---
+++ bin/number_method.dart
@@ -0,0 +1,32 @@
+void main() {
+  var x = -4;
+  print('x.isOdd: ${x.isOdd}');
+  print('x.isEven: ${x.isEven}');
+  print('x.abs(): ${x.abs()}');
+  print('x.ceil(): ${x.ceil()}');
+  print('x.floor(): ${x.floor()}');
+  print('x.remainder(2): ${x.remainder(2)}');
+  print('x.toDouble(): ${x.toDouble()}');
+  print('x.toInt(): ${x.toInt()}');
+  print('x.sign: ${x.sign}');
+  print('x.isNegative: ${x.isNegative}');
+  print('x.isFinite: ${x.isFinite}');
+  print('x.isInfinite: ${x.isInfinite}');
+  print('x.isNaN: ${x.isNaN}');
+  print('x.isNegative: ${x.isNegative}');
+
+  print('--------------------------------');
+
+  var y = 8.5;
+
+  print('y.round(): ${y.round()}');
+  print('y.truncate(): ${y.truncate()}');
+  print('y.toStringAsFixed(2): ${y.toStringAsFixed(2)}');
+  print('y.toStringAsExponential(): ${y.toStringAsExponential()}');
+  print('y.toStringAsPrecision(2): ${y.toStringAsPrecision(2)}');
+  print('y.clamp(0, 10): ${y.clamp(0, 10)}');
+  print('y.compareTo(10): ${y.compareTo(10)}');
+  print('y.isFinite: ${y.isFinite}');
+  print('y.isNaN: ${y.isNaN}');
+  print('y.isNegative: ${y.isNegative}');
+}
